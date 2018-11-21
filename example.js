c73a3881befce36952440f620293b03feab06d51
Chris Broski <chrisbroski@gmail.com>
2018-11-21 09:44:50 -0500

    Added key/value UI

--- example.js
+++ example.js
@@ -1,33 +1,56 @@
-const endure = require("./Endure");
+const Endure = require("./Endure");
 const http = require('http');
 const querystring = require('querystring');
-const data = new endure("data");
-if (!data.data) {
-    data.data = [];
+const data = new Endure();
+if (!data.array) {
+    data.array = [];
+}
+
+function htmlForm(data) {
+    return `<!doctype html>
+<html lang="en-us">
+<title>Data Test</title>
+<style>
+form {padding-bottom: 8px; }
+input {width: 80px; margin-right: 8px; }
+</style>
+
+<h1>Data Test</h1>
+<pre>${JSON.stringify(data, null, "    ")}</pre>
+<form action="" method="POST"><input name="array-push"><button>add to array</button></form>
+<form action="" method="POST"><input name="key">: <input name="val"><button>set key: value</button></form>
+`;
 }
 
 function routeMethods(req, rsp, body) {
     var requestBody;
     if (req.method === 'GET') {
         rsp.writeHead(200, {'Content-Type': 'text/html'});
-        rsp.end(`<!doctype html>
-        <html lang="en-us">
-        <title>Data Test</title>
-
-        <h1>Data Test</h1>
-        <pre>${JSON.stringify(data, null, "    ")}</pre>
-        <form action="" method="POST"><input name="data"><button>add data</button></form>
-`);
+        rsp.end(htmlForm(data));
         return;
     }
 
     if (req.method === 'POST') {
         requestBody = querystring.parse(body);
-        data.data.push(requestBody.data);
-        rsp.writeHead(200, {'Content-Type': 'text/plain'});
-        rsp.end(`Added data ${requestBody.data}`);
-        return;
+        if (requestBody) {
+            if (requestBody["array-push"]) {
+                data.array.push(requestBody["array-push"]);
+                rsp.writeHead(200, {'Content-Type': 'text/plain'});
+                rsp.end(`Added data '${requestBody["array-push"]}' to array.`);
+                return;
+            }
+            if (requestBody["key"] && requestBody["val"]) {
+                data[requestBody.key] = requestBody.val;
+                rsp.writeHead(200, {'Content-Type': 'text/plain'});
+                rsp.end(`Update key '${requestBody.key}' to '${requestBody.val}'.`);
+                return;
+            }
+        }
+        data.array.push(requestBody.data);
+        rsp.writeHead(400, {'Content-Type': 'text/plain'});
+        rsp.end(`No data found`);
     }
+
     rsp.writeHead(405, {'Content-Type': 'text/plain'});
     rsp.end(`Method "${req.method}" not allowed.`);
 }
